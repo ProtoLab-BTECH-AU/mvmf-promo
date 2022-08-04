57ae8cde5b5981b1631a2bb7f77c2110d6c67b7e
Matteo Campinoti <matteo.campinoti94@gmail.com>
2022-08-04 14:30:41 +0200

    src:Map - add preliminary map page

---
+++ src/pages/Map.tsx
@@ -0,0 +1,35 @@
+import {FC, useContext} from "react"
+import {Col, Container, Row} from "react-bootstrap"
+import {Header} from "../components/Header"
+import {LanguageContext, TLanguage} from "../context/LanguageContext"
+
+const text: Record<TLanguage, string[]> = {
+  danish: ["Kortet er foreløbigt, ændringer kan forekomme"],
+  english: ["The map is preliminary, changes may occur"],
+}
+
+const title: Record<TLanguage, string> = {
+  danish: "Kort",
+  english: "Map",
+}
+
+export const Map: FC = () => {
+  const languageContext = useContext(LanguageContext)
+
+  document.title = `MVMF 2022 | ${title[languageContext.language]}`
+
+  return <>
+    <Header title={title[languageContext.language]} backURl="/" xs={12} lg={7} xl={6}/>
+    <Container>
+      <Row>
+        <Col xs={12}>
+          {text[languageContext.language].map((p, i) => <p key={i}>{p}</p>)}
+        </Col>
+
+        <Col xs={12}>
+          <img src={`${process.env.PUBLIC_URL}/images/walking_tour_map.png`} alt=""/>
+        </Col>
+      </Row>
+    </Container>
+  </>
+}
