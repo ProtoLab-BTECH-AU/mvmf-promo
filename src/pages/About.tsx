761b02175a81aa165fab5f50b27a01838243a69a
Matteo Campinoti <matteo.campinoti94@gmail.com>
2022-08-05 12:07:18 +0200

    src:About - move maker definition below main text and reduce its font size

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -5,8 +5,11 @@ import {LanguageContext, TLanguage} from "../context/LanguageContext"
 
 const text: Record<TLanguage, (JSX.Element | string)[]> = {
   danish: [
-    <>
-      <h2 className="fw-bold">Hvad er en maker?</h2>
+    "Midtvest Maker Fest (MVMF) er en begivenhed, der støtter og hylder maker-kulturen som et fællesskab ved at forbinde det med lokalsamfund, industri og andre interesserede organisationer.",
+    "Godt håndværk inden for design, materialeprocesser eller teknologiledelse er fundamentet for at fremstille gode produkter, services og oplevelser. Maker-spaces og maker-fællesskaber hjælper ikke blot med at udbrede godt håndværk, men de skaber og forfiner også færdigheder i anvendelsen af nye digitale værktøjer som eksempelvis 3D-printere, laserskærere og mikrocontrollere. Som et netværk af maker-spaces i Midt- og Vestjylland er det vores mål at facilitere maker-færdigheder, viden og informationsdeling mellem borgere, virksomheder, uddannelsesinstitutioner og civile organisationer.",
+    "Som et tre-dages arrangement, der afholdes i Birk Centerpark i Herning, vil MVMF samle makere, lokale virksomheder, iværksættere og andre skabere for at dele erfaringer og fejre maker-kulturen og det gode håndværk med nye teknologier.",
+    <div className="mt-5" style={{fontSize: "70%"}}>
+      <h5 className="fw-bold">Hvad er en maker?</h5>
 
       Adam Savage beskriver det på en udførlig møde:<br/>
       <Figure className="mb-0">
@@ -20,22 +23,22 @@ const text: Record<TLanguage, (JSX.Element | string)[]> = {
           </a>
         </Figure.Caption>
       </Figure>
-    </>,
-    <>
+    </div>,
+    <div style={{fontSize: "70%"}}>
       For en akademisk beskrivelse af den "maker movement" se Dale Doughertys tekst:&nbsp;
       <a href="https://direct.mit.edu/itgg/article-pdf/7/3/11/704919/inov_a_00135.pdf">The Maker Movement</a>.
-    </>,
-    <>
+    </div>,
+    <div style={{fontSize: "70%"}}>
       For en mere generisk forståelse henvises til&nbsp;
       <a href="https://en.wikipedia.org/wiki/Maker_culture">artiklen om makerkultur på Wikipedia (på engelsk)</a>.
-    </>,
-    "Midtvest Maker Fest (MVMF) er en begivenhed, der støtter og hylder maker-kulturen som et fællesskab ved at forbinde det med lokalsamfund, industri og andre interesserede organisationer.",
-    "Godt håndværk inden for design, materialeprocesser eller teknologiledelse er fundamentet for at fremstille gode produkter, services og oplevelser. Maker-spaces og maker-fællesskaber hjælper ikke blot med at udbrede godt håndværk, men de skaber og forfiner også færdigheder i anvendelsen af nye digitale værktøjer som eksempelvis 3D-printere, laserskærere og mikrocontrollere. Som et netværk af maker-spaces i Midt- og Vestjylland er det vores mål at facilitere maker-færdigheder, viden og informationsdeling mellem borgere, virksomheder, uddannelsesinstitutioner og civile organisationer.",
-    "Som et tre-dages arrangement, der afholdes i Birk Centerpark i Herning, vil MVMF samle makere, lokale virksomheder, iværksættere og andre skabere for at dele erfaringer og fejre maker-kulturen og det gode håndværk med nye teknologier.",
+    </div>,
   ],
   english: [
-    <>
-      <h2 className="fw-bold">What is a maker?</h2>
+    "The Midtvest Maker Fest (MVMF) is an event that supports and celebrates maker culture with their communities by connecting them with local industry and institutions.",
+    "Good craftsmanship, in design, material processes or technology management is the backbone for manufacturing good products, services and experiences. Makerspaces and maker communities not only help inculcate good craftsmanship but they also facilitate the honing of skills with new digital tools like 3D printers, laser cutters and microcontrollers. Our aim as a network of makerspaces in midtvest Jutland is to facilitate maker skill, knowledge and information sharing between educational institutions, businesses, and civic organizations.",
+    "As a three day event at Birk Centerpark in Herning MVMF will bring together makers, students local companies, entrepreneurs, tool makers to share and celebrate making and good craftmanship with new technologies.",
+    <div className="mt-5" style={{fontSize: "70%"}}>
+      <h5 className="fw-bold">What is a maker?</h5>
 
       Adam Savage has worded this in an eloquent way:<br/>
       <Figure className="mb-0">
@@ -49,18 +52,15 @@ const text: Record<TLanguage, (JSX.Element | string)[]> = {
           </a>
         </Figure.Caption>
       </Figure>
-    </>,
-    <>
+    </div>,
+    <div style={{fontSize: "70%"}}>
       For an articulation of "maker movement" please refer to Dale Dougherty’s text:&nbsp;
       <a href="https://direct.mit.edu/itgg/article-pdf/7/3/11/704919/inov_a_00135.pdf">The Maker Movement</a>.
-    </>,
-    <>
+    </div>,
+    <div style={{fontSize: "70%"}}>
       For a more generic understanding of maker culture please refer to the&nbsp;
       <a href="https://en.wikipedia.org/wiki/Maker_culture">Maker culture article on Wikipedia</a>.
-    </>,
-    "The Midtvest Maker Fest (MVMF) is an event that supports and celebrates maker culture with their communities by connecting them with local industry and institutions.",
-    "Good craftsmanship, in design, material processes or technology management is the backbone for manufacturing good products, services and experiences. Makerspaces and maker communities not only help inculcate good craftsmanship but they also facilitate the honing of skills with new digital tools like 3D printers, laser cutters and microcontrollers. Our aim as a network of makerspaces in midtvest Jutland is to facilitate maker skill, knowledge and information sharing between educational institutions, businesses, and civic organizations.",
-    "As a three day event at Birk Centerpark in Herning MVMF will bring together makers, students local companies, entrepreneurs, tool makers to share and celebrate making and good craftmanship with new technologies.",
+    </div>,
   ],
 }
 
