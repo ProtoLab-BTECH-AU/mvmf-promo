d7fd7ca0761a83ee0105247b6abd64833f887674
Matteo Campinoti <matteo.campinoti94@gmail.com>
2022-08-06 13:34:32 +0200

    src:LanguageSelector - change to single toggle button on small screens

--- src/components/LanguageSelector.tsx
+++ src/components/LanguageSelector.tsx
@@ -4,17 +4,32 @@ import {LanguageContext} from "../context/LanguageContext"
 export const LanguageSelector = () => {
   const languageContext = useContext(LanguageContext)
 
-  return <div className="d-inline-flex justify-content-between">
-    <span className={languageContext.language === "danish" ? "fw-bold orange" : "pointer"}
-          style={{width: "2.5ch", textAlign: "center", display: "inline-block"}}
-          onClick={() => languageContext.change("danish")}>
-      DA
-    </span>
-    <span>|</span>
-    <span className={languageContext.language === "english" ? "fw-bold orange" : "pointer"}
-          style={{width: "2.5ch", textAlign: "center", display: "inline-block"}}
-          onClick={() => languageContext.change("english")}>
-      EN
-    </span>
-  </div>
+  return <>
+    <div className="d-none d-sm-inline-flex justify-content-between">
+      <span className={languageContext.language === "danish" ? "fw-bold orange" : "pointer"}
+            style={{width: "2.5ch", textAlign: "center", display: "inline-block"}}
+            onClick={() => languageContext.change("danish")}>
+        DA
+      </span>
+      <span>|</span>
+      <span className={languageContext.language === "english" ? "fw-bold orange" : "pointer"}
+            style={{width: "2.5ch", textAlign: "center", display: "inline-block"}}
+            onClick={() => languageContext.change("english")}>
+          EN
+        </span>
+    </div>
+
+    <div className="d-inline-flex d-sm-none justify-content-between pointer"
+         onClick={() => languageContext.change(languageContext.language === "english" ? "danish" : "english")}>
+      <span className={languageContext.language === "danish" ? "fw-bold orange" : ""}
+            style={{width: "2.5ch", textAlign: "center", display: "inline-block"}}>
+        DA
+      </span>
+      <span>|</span>
+      <span className={languageContext.language === "english" ? "fw-bold orange" : ""}
+            style={{width: "2.5ch", textAlign: "center", display: "inline-block"}}>
+        EN
+      </span>
+    </div>
+  </>
 }
