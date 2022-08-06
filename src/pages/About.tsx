19bccf67d581688ad87229aec33eda50ee004b1e
Matteo Campinoti <matteo.campinoti94@gmail.com>
2022-08-06 18:17:30 +0200

    src:About - improve margins, add link to source code

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -7,7 +7,7 @@ const text: Record<TLanguage, (JSX.Element | string)[]> = {
     "Midtvest Maker Fest (MVMF) er en begivenhed, der støtter og hylder maker-kulturen som et fællesskab ved at forbinde det med lokalsamfund, industri og andre interesserede organisationer.",
     "Godt håndværk inden for design, materialeprocesser eller teknologiledelse er fundamentet for at fremstille gode produkter, services og oplevelser. Maker-spaces og maker-fællesskaber hjælper ikke blot med at udbrede godt håndværk, men de skaber og forfiner også færdigheder i anvendelsen af nye digitale værktøjer som eksempelvis 3D-printere, laserskærere og mikrocontrollere. Som et netværk af maker-spaces i Midt- og Vestjylland er det vores mål at facilitere maker-færdigheder, viden og informationsdeling mellem borgere, virksomheder, uddannelsesinstitutioner og civile organisationer.",
     "Som et tre-dages arrangement, der afholdes i Birk Centerpark i Herning, vil MVMF samle makere, lokale virksomheder, iværksættere og andre skabere for at dele erfaringer og fejre maker-kulturen og det gode håndværk med nye teknologier.",
-    <div className="mt-5" style={{fontSize: "70%"}}>
+    <p className="mt-5" style={{fontSize: "70%"}}>
       <h5 className="fw-bold">Hvad er en maker?</h5>
 
       Adam Savage beskriver det på en udførlig møde:<br/>
@@ -22,25 +22,33 @@ const text: Record<TLanguage, (JSX.Element | string)[]> = {
           </a>
         </Figure.Caption>
       </Figure>
-    </div>,
-    <div style={{fontSize: "70%"}}>
+    </p>,
+    <p style={{fontSize: "70%"}}>
       For en akademisk beskrivelse af den "maker movement" se Dale Doughertys tekst:&nbsp;
-      <a target="_blank" rel="noreferrer" href="https://direct.mit.edu/itgg/article-pdf/7/3/11/704919/inov_a_00135.pdf">
+      <a target="_blank" rel="noreferrer"
+         href="https://direct.mit.edu/itgg/article-pdf/7/3/11/704919/inov_a_00135.pdf">
         The Maker Movement
       </a>.
-    </div>,
-    <div style={{fontSize: "70%"}}>
+      <br/>
       For en mere generisk forståelse henvises til&nbsp;
       <a target="_blank" rel="noreferrer" href="https://en.wikipedia.org/wiki/Maker_culture">
         artiklen om makerkultur på Wikipedia (på engelsk)
       </a>.
-    </div>,
+    </p>,
+    <p style={{fontSize: "70%"}}>
+      Denne hjemmeside er open source, og distribueres med en Creative Commons Attribution-ShareAlike licens. Koden kan
+      findes på ProtoLab's GitHub-side:&nbsp;
+      <a target="_blank" rel="noreferrer" className="text-nowrap"
+         href="https://github.com/ProtoLab-BTECH-AU/mvmf-promo">
+        mvmf-promo@GitHub.com
+      </a>
+    </p>,
   ],
   english: [
     "The Midtvest Maker Fest (MVMF) is an event that supports and celebrates maker culture with their communities by connecting them with local industry and institutions.",
     "Good craftsmanship, in design, material processes or technology management is the backbone for manufacturing good products, services and experiences. Makerspaces and maker communities not only help inculcate good craftsmanship but they also facilitate the honing of skills with new digital tools like 3D printers, laser cutters and microcontrollers. Our aim as a network of makerspaces in midtvest Jutland is to facilitate maker skill, knowledge and information sharing between educational institutions, businesses, and civic organizations.",
     "As a three day event at Birk Centerpark in Herning MVMF will bring together makers, students local companies, entrepreneurs, tool makers to share and celebrate making and good craftmanship with new technologies.",
-    <div className="mt-5" style={{fontSize: "70%"}}>
+    <p className="mt-5" style={{fontSize: "70%"}}>
       <h5 className="fw-bold">What is a maker?</h5>
 
       Adam Savage has worded this in an eloquent way:<br/>
@@ -55,18 +63,25 @@ const text: Record<TLanguage, (JSX.Element | string)[]> = {
           </a>
         </Figure.Caption>
       </Figure>
-    </div>,
-    <div style={{fontSize: "70%"}}>
+    </p>,
+    <p style={{fontSize: "70%"}}>
       For an articulation of "maker movement" please refer to Dale Dougherty’s text:&nbsp;
       <a target="_blank" rel="noreferrer" href="https://direct.mit.edu/itgg/article-pdf/7/3/11/704919/inov_a_00135.pdf">
         The Maker Movement</a>.
-    </div>,
-    <div style={{fontSize: "70%"}}>
+      <br/>
       For a more generic understanding of maker culture please refer to the&nbsp;
       <a target="_blank" rel="noreferrer" href="https://en.wikipedia.org/wiki/Maker_culture">
         Maker culture article on Wikipedia
       </a>.
-    </div>,
+    </p>,
+    <p style={{fontSize: "70%"}}>
+      This webpage is open source and is distributed with a Creative Commons Attribution-ShareAlike licence. The code
+      can be found on the ProtoLab's GitHub page:&nbsp;
+      <a target="_blank" rel="noreferrer" className="text-nowrap"
+         href="https://github.com/ProtoLab-BTECH-AU/mvmf-promo">
+        mvmf-promo@GitHub.com
+      </a>
+    </p>,
   ],
 }
 
