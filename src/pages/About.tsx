11d6e7cc1c53e59e0e696cbc63c85918a4738cec
Matteo Campinoti <matteo.campinoti94@gmail.com>
2022-08-17 10:09:29 +0200

    src:About - update text

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -6,11 +6,11 @@ const text: Record<TLanguage, (JSX.Element | string)[]> = {
   danish: [
     "Midtvest Maker Fest (MVMF) er en begivenhed, der støtter og hylder maker-kulturen som et fællesskab ved at forbinde det med lokalsamfund, industri og andre interesserede organisationer.",
     "Godt håndværk inden for design, materialeprocesser eller teknologiledelse er fundamentet for at fremstille gode produkter, services og oplevelser. Maker-spaces og maker-fællesskaber hjælper ikke blot med at udbrede godt håndværk, men de skaber og forfiner også færdigheder i anvendelsen af nye digitale værktøjer som eksempelvis 3D-printere, laserskærere og mikrocontrollere. Som et netværk af maker-spaces i Midt- og Vestjylland er det vores mål at facilitere maker-færdigheder, viden og informationsdeling mellem borgere, virksomheder, uddannelsesinstitutioner og civile organisationer.",
-    "Som et tre-dages arrangement, der afholdes i Birk Centerpark i Herning, vil MVMF samle makere, lokale virksomheder, iværksættere og andre skabere for at dele erfaringer og fejre maker-kulturen og det gode håndværk med nye teknologier.",
+    "Som et tre-dages arrangement, der afholdes hos Aarhus Universitetet i Herning, vil MVMF samle makers, lokale virksomheder, iværksættere og andre skabere for at dele erfaringer og fejre maker-kulturen og det gode håndværk med nye teknologier.",
     <p className="mt-5" style={{fontSize: "70%"}}>
       <h5 className="fw-bold">Hvad er en maker?</h5>
 
-      Adam Savage beskriver det på en udførlig møde:<br/>
+      Adam Savage beskriver det på en udførlig måde:<br/>
       <Figure className="mb-0">
         <blockquote className="blockquote fst-italic">
           Humans do two things that make us unique from all other animals; we use tools and we tell stories. And when
@@ -51,7 +51,7 @@ const text: Record<TLanguage, (JSX.Element | string)[]> = {
   english: [
     "The Midtvest Maker Fest (MVMF) is an event that supports and celebrates maker culture with their communities by connecting them with local industry and institutions.",
     "Good craftsmanship, in design, material processes or technology management is the backbone for manufacturing good products, services and experiences. Makerspaces and maker communities not only help inculcate good craftsmanship but they also facilitate the honing of skills with new digital tools like 3D printers, laser cutters and microcontrollers. Our aim as a network of makerspaces in midtvest Jutland is to facilitate maker skill, knowledge and information sharing between educational institutions, businesses, and civic organizations.",
-    "As a three day event at Birk Centerpark in Herning MVMF will bring together makers, students local companies, entrepreneurs, tool makers to share and celebrate making and good craftmanship with new technologies.",
+    "As a three day event at Aarhus University in Herning MVMF will bring together makers, students local companies, entrepreneurs, tool makers to share and celebrate making and good craftmanship with new technologies.",
     <p className="mt-5" style={{fontSize: "70%"}}>
       <h5 className="fw-bold">What is a maker?</h5>
 
