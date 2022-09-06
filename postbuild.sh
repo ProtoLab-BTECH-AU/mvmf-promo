a7a98f9f162189bc2cec20b29902dd264f1f4a54
Matteo Campinoti <matteo.campinoti94@gmail.com>
2022-09-06 13:41:29 +0200

    postbuild - add for-companies and for-students

--- postbuild.sh
+++ postbuild.sh
@@ -1,4 +1,4 @@
-pages=("about" "program" "map" "challenge" "challenge1" "challenge2" "challenge3")
+pages=("about" "for-companies" "for-students" "program" "map" "challenge" "challenge1" "challenge2" "challenge3")
 
 for page in "${pages[@]}" ; do
     mkdir "build/$page"
