99bc8fff2b3c650471477fcd7374ef9d95266021
Matteo Campinoti <matteo.campinoti94@gmail.com>
2022-09-28 13:56:54 +0200

    postbuild - add images

--- postbuild.sh
+++ postbuild.sh
@@ -1,4 +1,4 @@
-pages=("about" "for-companies" "for-students" "program" "map" "challenge" "challenge1" "challenge2" "challenge3" "workshops" "blob" "blob-titles")
+pages=("about" "for-companies" "for-students" "program" "map" "challenge" "challenge1" "challenge2" "challenge3" "workshops" "blob" "blob-titles" "images")
 
 for page in "${pages[@]}" ; do
     mkdir "build/$page"
